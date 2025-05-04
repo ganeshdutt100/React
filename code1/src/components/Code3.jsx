3793202252759c241f37bd7d0191df972b23ae34
ganeshdutt <gdutt2304@gmail.com>
2025-05-04 17:20:13 +0530

    update date and time

--- code1/src/components/Code3.jsx
+++ code1/src/components/Code3.jsx
@@ -8,11 +8,21 @@ const Code3 = () => {
     }, 1000);
     return () => clearInterval(timer);
   }, []);
+  const addZero = (num) => {
+    return num < 10 ? "0" + num : num;
+  };
+  const hours = addZero(time.getHours());
+  const minutes = addZero(time.getMinutes());
+  const seconds = addZero(time.getSeconds());
 
   return (
     <div>
       <h2>Clock </h2>
       <h1>{time.toLocaleDateString()}</h1>
+
+      <div>
+        {hours} : {minutes}: {seconds}
+      </div>
     </div>
   );
 };
